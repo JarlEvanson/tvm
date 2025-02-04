267eb06cdd0af55f245fbca30b28cf24c6ea017a
Jarl Evanson <evanson.jarl@gmail.com>
2025-02-04 17:25:57 -0600

    `limine`: Add feature request delimiters

--- common/limine/src/lib.rs
+++ common/limine/src/lib.rs
@@ -2,6 +2,17 @@
 
 #![no_std]
 
+/// Marks the start of the Limine boot protocol feature requests section.
+pub const REQUESTS_START_MARKER: [u64; 4] = [
+    0xf6b8f4b39de7d1ae,
+    0xfab91a6940fcb9cf,
+    0x785c6ed015d3e316,
+    0x181e920a7852b9d9,
+];
+
+/// Marks the end of the Limine boot protocol feature requests section.
+pub const REQUESTS_END_MARKER: [u64; 2] = [0xadc0e0531bb10d03, 0x9572709f31764c62];
+
 /// The base revision of the Limine boot protocol this crate provides.
 pub const BASE_REVISION: u64 = 3;
 
