0dad8cb6c476fe70b8381ccb23ea3650554ae683
Jarl Evanson <evanson.jarl@gmail.com>
2025-02-05 09:23:20 -0600

    `limine`: Add RSDP feature

--- common/limine/src/lib.rs
+++ common/limine/src/lib.rs
@@ -12,6 +12,7 @@ pub mod memory_map;
 pub mod module;
 pub mod mp;
 pub mod paging_mode;
+pub mod rsdp;
 pub mod stack_size;
 
 /// Marks the start of the Limine boot protocol feature requests section.
