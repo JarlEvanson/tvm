3565ebba3b7b5d47571f4231061cc2792f4d85dc
Jarl Evanson <evanson.jarl@gmail.com>
2025-02-10 08:50:45 -0600

    `limine`: Implement `BaseRevisionTag`

--- common/limine/src/lib.rs
+++ common/limine/src/lib.rs
@@ -1,3 +1,31 @@
 //! Definitions and implementations of the Limine boot protocol.
 
 #![no_std]
+
+/// The base revision of the Limine boot protocol this crate provides.
+pub const BASE_REVISION: u64 = 3;
+
+/// 1st magic number indicating the base revision tag.
+pub const BASE_REVISION_MAGIC_0: u64 = 0xf9562b2d5c95a6c8;
+/// 2nd magic number indicating the base revision tag.
+pub const BASE_REVISION_MAGIC_1: u64 = 0x6a7b384944536bdc;
+
+/// A tag setting the minimum base revision of the Limine boot protocol supported by the
+/// executable and containing the base revision of the Limine boot protocol to load the
+/// executable.
+#[repr(C)]
+#[derive(Clone, Copy, Debug, Hash, PartialEq, Eq, PartialOrd, Ord)]
+pub struct BaseRevisionTag {
+    /// Magic number.
+    pub magic: u64,
+    /// The base revision of the Limine boot protocol used to load the executable.
+    ///
+    /// This field may not contain the correct revision if the bootloader does not support base
+    /// revision 3.
+    pub loaded_revision: u64,
+
+    /// The application's expected base revision.
+    ///
+    /// This must be set to 0 if the executable's base revision is supported by the bootloader.
+    pub supported_revision: u64,
+}
