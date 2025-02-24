4bb017595ba363b8214b9f23e2e2909e8b827d96
Jarl Evanson <evanson.jarl@gmail.com>
2025-02-24 10:03:48 -0600

    `tvm_loader`: Begin core loader support library

---
+++ tvm_loader/loader/src/lib.rs
@@ -0,0 +1,8 @@
+//! Core implementation of `tvm_loader`.
+//!
+//! This crate implements the core features and interfaces of `tvm_loader` and system-independent
+//! code that many other architecture, platform, and system crates utilize.
+
+#![no_std]
+
+extern crate alloc;
