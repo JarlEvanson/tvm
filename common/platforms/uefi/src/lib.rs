962f621245035954cadcc96384d270244674cbfa
Jarl Evanson <evanson.jarl@gmail.com>
2025-02-24 11:59:56 -0600

    `uefi`: Begin raw UEFI support library

---
+++ common/platforms/uefi/src/lib.rs
@@ -0,0 +1,6 @@
+//! Raw interface for working with UEFI.
+//!
+//! This crate is intended to provide the definitions required to build more sophisticated crates
+//! with which end-users should work.
+
+#![no_std]
