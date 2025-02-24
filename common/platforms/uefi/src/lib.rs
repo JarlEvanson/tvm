038aaa202dd8f323f44dcbe74a100bfb42e485fa
Jarl Evanson <evanson.jarl@gmail.com>
2025-02-24 11:59:59 -0600

    `uefi`: Add definitions of memory structures

--- common/platforms/uefi/src/lib.rs
+++ common/platforms/uefi/src/lib.rs
@@ -5,4 +5,25 @@
 
 #![no_std]
 
+use core::fmt;
+
 pub mod data_types;
+pub mod memory;
+
+/// Forces formatting to be carried out as [`LowerHex`][flh].
+///
+/// [flh]: fmt::LowerHex
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub(crate) struct FmtLowerHex<T: fmt::LowerHex>(T);
+
+impl<T: fmt::LowerHex> fmt::Debug for FmtLowerHex<T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        <T as fmt::LowerHex>::fmt(&self.0, f)
+    }
+}
+
+impl<T: fmt::LowerHex> fmt::Display for FmtLowerHex<T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        <T as fmt::LowerHex>::fmt(&self.0, f)
+    }
+}
