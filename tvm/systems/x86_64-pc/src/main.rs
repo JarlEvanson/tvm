ff1a154017ab4ef1bd432ada5beba2d76bdd265e
Jarl Evanson <evanson.jarl@gmail.com>
2025-03-01 13:45:59 -0600

    `tvm-x86_64-generic`: Implement `tvm` for `x86_64` PC platforms

---
+++ tvm/systems/x86_64-pc/src/main.rs
@@ -0,0 +1,21 @@
+//! Implementation of `tvm` for `x86_64` PC systems.
+
+#![no_std]
+#![no_main]
+
+use boot_info::BootInfo;
+
+/// Test message.
+static MESSAGE: &str = "Test";
+
+#[unsafe(no_mangle)]
+extern "C" fn _start(boot_info: *mut BootInfo) -> u32 {
+    unsafe { ((*boot_info).write)(MESSAGE.as_ptr(), MESSAGE.len()) }
+
+    todo!()
+}
+
+#[panic_handler]
+fn panic_handler(_: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
