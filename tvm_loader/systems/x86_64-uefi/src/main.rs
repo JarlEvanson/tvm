b437631c408dc5ddf9071b634d24a9f88732991c
Jarl Evanson <evanson.jarl@gmail.com>
2025-03-04 13:35:43 -0600

    `tvm_loader-x86_64-uefi`: Implement multi-arch support

--- tvm_loader/systems/x86_64-uefi/src/main.rs
+++ tvm_loader/systems/x86_64-uefi/src/main.rs
@@ -4,22 +4,50 @@
 #![no_main]
 
 use tvm_loader::{
-    elf_loader::{Machine, get_machine},
+    elf_loader::{Machine, get_machine, load_application},
     log_error,
 };
 use tvm_loader_uefi::unsafe_entry_point;
+use tvm_loader_x86::paging::pae::PaePageTables;
+use tvm_loader_x86_64::X86_64PageTable;
 
 extern crate tvm_loader_uefi;
 
 unsafe_entry_point!(main);
 
 fn main() -> Result<(), tvm_loader_uefi::Status> {
-    let embedded_image = tvm_loader_uefi::embedded::get_tvm_image();
+    let mut switch_space =
+        X86_64PageTable::new_current().expect("failed to construct switch tables");
 
+    let embedded_image = tvm_loader_uefi::embedded::get_tvm_image();
     let machine = get_machine(embedded_image).expect("invalid embedded tvm image");
     match machine {
-        Machine::INTEL_386 => todo!(),
-        Machine::X86_64 => todo!(),
+        Machine::INTEL_386 => {
+            let mut application_space =
+                PaePageTables::new_max_supported().expect("failed to construct application tables");
+
+            let result = load_application(
+                embedded_image,
+                Machine::INTEL_386,
+                &mut application_space,
+                tvm_loader_x86::handle_relocation,
+            );
+
+            let entry_point = result.expect("failed to load tvm image");
+        }
+        Machine::X86_64 => {
+            let mut application_space = X86_64PageTable::new_max_supported()
+                .expect("failed to construct application tables");
+
+            let result = load_application(
+                embedded_image,
+                Machine::X86_64,
+                &mut application_space,
+                tvm_loader_x86_64::handle_relocation,
+            );
+
+            let entry_point = result.expect("failed to load tvm image");
+        }
         machine => unimplemented!("{machine:?} is not supported"),
     }
 
