a142a34dc7793a87007bd7292c704f7a8cea6bb7
Jarl Evanson <evanson.jarl@gmail.com>
2025-03-06 16:16:35 -0800

    `xtask`: Define `Loader` description struct

---
+++ xtask/src/loader/mod.rs
@@ -0,0 +1,70 @@
+//! Definitions of the supported `tvm_loader` system configurations.
+
+use crate::Arch;
+
+/// List of all of the `tvm_loader` system crates.
+pub static LOADERS: &[&Loader] = &[];
+
+/// Returns the [`Loader`] associated with the given [`Arch`] and [`Platform`].
+pub fn lookup(arch: Arch, platform: Platform) -> Option<&'static Loader> {
+    LOADERS
+        .iter()
+        .copied()
+        .find(|loader| loader.arch == arch && loader.platform == platform)
+}
+
+/// The description of a `tvm_loader` system crate.
+#[derive(Clone, Copy, Debug, Hash, PartialEq, Eq)]
+pub struct Loader {
+    /// The name of the `tvm_loader` system.
+    pub name: &'static str,
+
+    /// The architecture this `tvm_loader` system supports.
+    pub arch: Arch,
+    /// The platform this `tvm_loader` system supports.
+    pub platform: Platform,
+    /// The target string passed to `cargo` when building.
+    ///
+    /// This may be a built-in target or a path to custom JSON target file.
+    pub target: &'static str,
+    /// The valid features for this `tvm_loader` crate.
+    pub features: &'static [&'static str],
+
+    /// Additional arguments that must be passed to `cargo` when building.
+    pub additional_build_arguments: &'static [&'static str],
+}
+
+/// The architectures supported by `tvm_loader`.
+#[derive(Clone, Copy, Debug, Hash, PartialEq, Eq)]
+pub enum Platform {
+    /// The UEFI firmware environment.
+    Uefi,
+}
+
+impl Platform {
+    /// Returns the textual representation of the [`Platform`].
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Self::Uefi => "uefi",
+        }
+    }
+
+    /// Returns the file extension associated with the platform.
+    pub fn file_extension(&self) -> Option<&'static str> {
+        match self {
+            Self::Uefi => Some("efi"),
+        }
+    }
+}
+
+impl clap::ValueEnum for Platform {
+    fn value_variants<'a>() -> &'a [Self] {
+        static PLATFORMS: &[Platform] = &[Platform::Uefi];
+
+        PLATFORMS
+    }
+
+    fn to_possible_value(&self) -> Option<clap::builder::PossibleValue> {
+        Some(clap::builder::PossibleValue::new(self.as_str()))
+    }
+}
--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -2,6 +2,39 @@
 
 use anyhow::Result;
 
+pub mod loader;
+
 fn main() -> Result<()> {
     Ok(())
+}
+
+/// The architectures supported by `tvm`.
+#[derive(Clone, Copy, Debug, Hash, PartialEq, Eq)]
+pub enum Arch {
+    /// The `x86` architecture.
+    X86,
+    /// The `x86_64` architecture.
+    X86_64,
+}
+
+impl Arch {
+    /// Returns the textual representation of the [`Arch`].
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Self::X86 => "x86",
+            Self::X86_64 => "x86_64",
+        }
+    }
+}
+
+impl clap::ValueEnum for Arch {
+    fn value_variants<'a>() -> &'a [Self] {
+        static ARCHITECTURES: &[Arch] = &[Arch::X86, Arch::X86_64];
+
+        ARCHITECTURES
+    }
+
+    fn to_possible_value(&self) -> Option<clap::builder::PossibleValue> {
+        Some(clap::builder::PossibleValue::new(self.as_str()))
+    }
 }
